12e986cfd7614ea3d5930a44dbe11c9ac2fe5c90
chriskhoward <120136770+chriskhoward@users.noreply.github.com>
2026-03-09 11:56:21 -0500

    fix: update CSP to allow Clerk custom domain, Fillout, and GTM scripts

--- next.config.ts
+++ next.config.ts
@@ -54,11 +54,11 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://*.clerk.accounts.dev https://accounts.clerk.com https://js.clerk.com https://*.sanity-cdn.com https://*.sanity.io",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://*.google-analytics.com https://*.clerk.accounts.dev https://accounts.clerk.com https://js.clerk.com https://*.clerk.com https://*.sanity-cdn.com https://*.sanity.io https://*.fillout.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "img-src 'self' data: blob: https://cdn.sanity.io https://img.clerk.com https://img.youtube.com https://i.ytimg.com https://vumbnail.com https://lh3.googleusercontent.com",
-              "frame-src 'self' https://www.youtube.com https://player.vimeo.com https://*.clerk.accounts.dev https://accounts.clerk.com https://js.clerk.com",
-              "connect-src 'self' https://*.clerk.com https://*.clerk.accounts.dev https://*.sanity.io https://*.apicdn.sanity.io https://*.sanity-cdn.com https://www.google-analytics.com",
+              "img-src 'self' data: blob: https://cdn.sanity.io https://img.clerk.com https://img.youtube.com https://i.ytimg.com https://vumbnail.com https://lh3.googleusercontent.com https://*.google-analytics.com https://*.googletagmanager.com",
+              "frame-src 'self' https://www.youtube.com https://player.vimeo.com https://*.clerk.accounts.dev https://accounts.clerk.com https://js.clerk.com https://*.clerk.com https://*.fillout.com",
+              "connect-src 'self' https://*.clerk.com https://*.clerk.accounts.dev https://*.sanity.io https://*.apicdn.sanity.io https://*.sanity-cdn.com https://www.google-analytics.com https://*.google-analytics.com https://*.googletagmanager.com https://*.fillout.com",
               "font-src 'self' data: https://fonts.gstatic.com",
               "worker-src 'self' blob:",
             ].join('; ')
